4eea8b24b68e317c9eead65a5ed587116dd1d3fd
ChristianTC <christiantambo15@gmail.com>
2022-06-17 21:38:37 -0400

    form with custom hook

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 
 import "./index.css"
 
@@ -12,5 +13,6 @@ root.render(
   // <HookApp />
   // <CounterApp />
   // <CounterWithCustomHook />
-  <SimpleForm />
+  // <SimpleForm />
+  <FormWithCustomHook />
 );
