7109fcd74b4c40957a6fa21745555def3f082a68
ChristianTC <christiantambo15@gmail.com>
2022-06-17 20:32:08 -0400

    useEffect - simple form

--- src/HookApp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const HookApp = () => {
-  return (
-    <div>HookApp</div>
-  )
-}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { SimpleForm } from './02-useEffect/SimpleForm';
+
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
