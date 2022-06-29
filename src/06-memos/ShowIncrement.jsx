8cbc1cec2539fc5ab9ed404acfc88a28fc8f517c
ChristianTC <christiantambo15@gmail.com>
2022-06-29 11:30:51 -0400

    useCallback

---
+++ src/06-memos/ShowIncrement.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const ShowIncrement = React.memo(({increment}) => {
+    console.log('back generate :(');
+
+    return (
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          increment()
+        }}
+      >
+        Increment
+      </button>
+    )
+  }
+)
--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import ReactDOM from 'react-dom/client';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+// import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 
 import "./index.css"
 
@@ -24,5 +25,6 @@ root.render(
   // <FocusScreen />
   // <Layout />
   // <Memorize />
-  <MemoHook />
+  // <MemoHook />
+  <CallbackHook />
 );
